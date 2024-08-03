0cea92d1276850990139864577ec6bea6bf4a307
SharkMC <160005502+SharkMC@users.noreply.github.com>
2024-08-04 08:45:09 +0900

    説明

--- WelcomeMessage/src/WelcomeMessage/Main.php
+++ WelcomeMessage/src/WelcomeMessage/Main.php
@@ -1,23 +1,21 @@
 <?php
 
-namespace WelcomeMessage;
+namespace WelcomeMessage; //ファイルの名前を設定
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
 
 class Main extends PluginBase implements Listener {
 
-    public function onEnable(): void {
+    public function onEnable(): void { //プラグインが有効になった時に実行
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onJoin(PlayerJoinEvent $event) {
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        $player->sendMessage("§e§lWelcome to §fserver name§e!");
-        $event->setJoinMessage("");
+    public function onJoin(PlayerJoinEvent $event) { //プレイヤーがサーバーに参加した時に実行
+        $player = $event->getPlayer(); //イベントを実行したプレイヤー情報を取得
+        $player->sendMessage("§e§lWelcome to §fserver name§e!"); //イベントを実行したプレイヤーに個別にメッセージを送信
+        $event->setJoinMessage(""); //参加した時のメッセージを空に設定
     }
 }
